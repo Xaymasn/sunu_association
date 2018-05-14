b7928b3357cedcfeb83f2bee48488a5869b8b620
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-14 13:12:15 +0000

    Correction

--- models/section.py
+++ models/section.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class Section(models.Model):
-    _name = 'sunu_association.section'
+    _name = 'sunuAssociation.section'
     nom = fields.Char( string='Nom', required=True, )
     zoneGeographique = fields.Text(
         string='Zone Géographique',
